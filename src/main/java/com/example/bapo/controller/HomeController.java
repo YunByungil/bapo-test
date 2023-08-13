98e17bee103f22b769efd7c2e800873096f8d35b
YunByungil <dbsquddlfz@naver.com>
2023-08-13 14:43:18 +0900

    환경변수 추가

--- src/main/java/com/example/bapo/controller/HomeController.java
+++ src/main/java/com/example/bapo/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.example.bapo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${my-env.first}")
+    private String first;
+
+    @Value("${my-env.second}")
+    private Integer second;
     @GetMapping
     public String home() {
+        System.out.println("first = " + first);
+        System.out.println("second = " + second);
         return "index";
     }
 }

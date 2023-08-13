2f4e89bef447067095a708f059e7d73ae0591d0a
YunByungil <dbsquddlfz@naver.com>
2023-08-13 12:39:35 +0900

    first commit

---
+++ src/main/java/com/example/bapo/BapoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bapo/BapoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

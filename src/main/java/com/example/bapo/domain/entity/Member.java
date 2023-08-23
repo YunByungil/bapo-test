835e1af832827baa3421926d86c36d9cd5cb34ad
YunByungil <dbsquddlfz@naver.com>
2023-08-23 17:28:45 +0900

    수정

--- src/main/java/com/example/bapo/domain/entity/Member.java
+++ src/main/java/com/example/bapo/domain/entity/Member.java
@@ -1,7 +1,17 @@
 package com.example.bapo.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String password;
